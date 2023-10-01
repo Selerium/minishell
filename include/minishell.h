c28d03402ba2db047eb5df3f186ce180c1c0c8c2
jens8n <jensen.bucoy@gmail.com>
2023-10-01 19:04:02 +0400

    QUOTATIONS WIP NEED TO HANDLE DIFFERENT TYPES OF QUOTES

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jebucoy <jebucoy@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 15:58:17 by jadithya          #+#    #+#             */
-/*   Updated: 2023/09/28 19:02:26 by jebucoy          ###   ########.fr       */
+/*   Updated: 2023/10/01 19:00:31 by jebucoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,5 +118,5 @@ void	expand_tokens(char **args, t_minishell shell);
 // quotes
 int		is_quotes_closed(int qflag, char q);
 char	*trim_quotes(char *input);
-char	*eliminate_quotes(char *input, size_t *idx, int qflag, size_t start);
+char	*eliminate_quotes(char *input, size_t *idx);
 #endif
--- src/quotes/quotes.c
+++ src/quotes/quotes.c
@@ -6,16 +6,16 @@
 /*   By: jebucoy <jebucoy@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 18:54:48 by jebucoy           #+#    #+#             */
-/*   Updated: 2023/09/29 18:37:23 by jebucoy          ###   ########.fr       */
+/*   Updated: 2023/10/01 18:59:47 by jebucoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// qflag = 0
-// if single quote (\') is encounted, set it to one
-// if double quote (\") set it to 2
-// if a closing quote of the specific is encountered, reset to 0
+/* qflag = 0
+if single quote (\') is encounted, set it to one
+if double quote (\") set it to 2
+if a closing quote of the specific is encountered, reset to 0*/
 int	is_quotes_closed(int qflag, char q)
 {
 	if (q == SINGLE && qflag == 0)
@@ -29,37 +29,50 @@ int	is_quotes_closed(int qflag, char q)
 	return (qflag);
 }
 
-char	*eliminate_quotes(char *input, size_t *idx, int qflag, size_t start)
+// char	*eliminate_quotes(char *input, size_t *idx, int qflag)
+// {
+// 	char	*tmp1;
+// 	char	*tmp2;
+// 	char	*tmp3;
+// 	char	*final;
+
+// 	printf("1 input[%zd] : [%c] | qflag value: %d\n", *idx, input[*idx], qflag);
+// 	tmp1 = ft_substr(input, 0, (*idx));
+// 	// start = *idx;
+// 	// (*idx)++;
+// 	// while (input[*idx] && qflag != 0)
+// 	// {
+// 	// 	qflag = is_quotes_closed(qflag, input[*idx]);
+// 	// 	printf("qflag = %d\n", qflag);
+// 	// 	if (qflag == 0)
+// 	// 		break ;
+// 	// 	(*idx)++;
+// 	// }
+// 	tmp2 = ft_substr(input, start + 1, (*idx) - (start + 1));
+// 	printf("tmp2: [%s]\n", tmp2);
+// 	tmp3 = ft_substr(input, (*idx + 1), ft_strlen(input) - (*idx));
+// 	printf("tmp3: [%s]\n", tmp3);
+// 	final = ft_strjoin(tmp1, tmp2);
+// 	free (input);
+// 	input = ft_strjoin(final, tmp3);
+// 	free(tmp1);
+// 	free(tmp2);
+// 	free(tmp3);
+// 	free(final);
+// 	// (*idx)--;
+// 	return (input);
+// }
+
+char	*eliminate_quotes(char *input, size_t *idx)
 {
 	char	*tmp1;
 	char	*tmp2;
-	char	*tmp3;
-	char	*final;
 
-	// (void)qflag;
-	printf("*idx: %zd\n", *idx);
-	if (start == 0)
-		tmp1 = ft_strdup("");
-	else
-		tmp1 = ft_substr(input, 0, (*idx));
-	printf("tmp1: [%s]\n", tmp1);
-	start = *idx;
-	printf("input[%zd] before while loop: [%c]\n", *idx, input[*idx]);
-	// (*idx) += ft_strlen(input) - (ft_strlen(tmp1) + 2);
-	while (input[*idx] && qflag != 0)
-		(*idx)++;
-	printf("input[%zd] after while loop: [%c]\n", *idx, input[*idx]);
-	tmp2 = ft_substr(input, start + 1, (*idx));
-	printf("tmp2: [%s]\n", tmp2);
-	tmp3 = ft_substr(input, (*idx + 1), ft_strlen(input) - (*idx));
-	printf("tmp3: [%s]\n", tmp3);
-	final = ft_strjoin(tmp1, tmp2);
+	tmp1 = ft_substr(input, 0, (*idx));
+	tmp2 = ft_substr(input, *idx + 1, ft_strlen(input) - (*idx) - 1);
 	free (input);
-	input = ft_strjoin(final, tmp3);
-	free(tmp1);
-	free(tmp2);
-	free(tmp3);
-	free(final);
+	input = ft_strjoin(tmp1, tmp2);
+	(*idx)--;
 	return (input);
 }
 
@@ -68,21 +81,22 @@ char	*trim_quotes(char *input)
 	size_t	i;
 	size_t	start;
 	int		qflag;
+	int		old_qflag;
 
 	i = 0;
 	start = 0;
 	qflag = 0;
 	while (input[i])
 	{
+		old_qflag = qflag;
 		qflag = is_quotes_closed(qflag, input[i]);
-		printf("input[%zd]: [%c] | qflag value: %d\n", i, input[i], qflag);
-		if (qflag != 0)
+		if (qflag != 0 || (!qflag && old_qflag))
 		{
 			start = i;
-			// while (input[i] && qflag != 0)
-			// 	i++;
-			input = eliminate_quotes(input, &i, qflag, start);
+			input = eliminate_quotes(input, &i);
+			qflag = 0;
 		}
+		printf("input[%zd] = %c\n", i, input[i]);
 		i++;
 	}
 	return (input);
