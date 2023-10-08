b5545bf97c1c83feba0ace42b6dfa81eb3f3c874
John Adithya <jadithya@42student.abudhabi.ae>
2023-10-08 18:19:49 +0400

    env done

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 19:40:20 by jebucoy           #+#    #+#             */
-/*   Updated: 2023/10/08 17:58:24 by jadithya         ###   ########.fr       */
+/*   Updated: 2023/10/08 18:19:24 by jadithya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,8 @@ bool	check_assignment(char *cmd)
 
 void	run_env(t_minishell *shell, bool parent)
 {
-	int	i;
-
-	i = 1;
 	if (!shell->cmds->cmd[1])
 		print_envs(shell->envs, true);
-	else
-		while (shell->cmds->cmd[i])
-			if (check_assignment(shell->cmds->cmd[i++]))
-				run_export(shell->cmds->cmd[i - 1], shell, parent, true);
-	print_envs(shell->envs, true);
 	if (!parent)
 		free_envs(shell->envs);
 }
