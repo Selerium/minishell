f26def2c5f8305cd2f2dff276baecce088463fcd
John Adithya <jadithya@42student.abudhabi.ae>
2023-10-09 20:55:37 +0400

    fixed empty pipes

--- src/parsing/syntax_check.c
+++ src/parsing/syntax_check.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   syntax_check.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jebucoy <jebucoy@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 22:20:23 by jebucoy           #+#    #+#             */
-/*   Updated: 2023/10/09 18:46:31 by jebucoy          ###   ########.fr       */
+/*   Updated: 2023/10/09 20:55:22 by jadithya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,30 @@ bool	print_error(t_flag flag)
 	return (false);
 }
 
+bool	check_empty_pipes(char *input)
+{
+	int		i;
+	bool	flag;
+
+	i = 0;
+	flag = false;
+	while (input && input[i])
+	{
+		if (flag && input[i] == '|')
+		{
+			printf("syntax error: pipes\n");
+			g_exitcode = 258;
+			return (false);
+		}
+		else if (input[i] == '|')
+			flag = true;
+		else if (!ft_isspace(input[i]))
+			flag = false;
+		i++;
+	}
+	return (true);
+}
+
 bool	is_syntax_valid(char *input)
 {
 	size_t	i;
@@ -36,6 +60,8 @@ bool	is_syntax_valid(char *input)
 	flag.quote = true;
 	flag.redir = true;
 	flag.pipe = true;
+	if (!check_empty_pipes(input))
+		return (false);
 	while (input[i])
 	{
 		if (input[i] == '"' || input[i] == '\'')
